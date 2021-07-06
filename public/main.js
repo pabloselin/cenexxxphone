49d254eff0aa1ede6204a943830e2a9b9902f3dc
Glitch (peerjs-video) <none>
2021-07-06 14:57:07 +0200

    check process

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@
 
 let Peer = window.Peer;
 let callerID = process.env.CALLER_ID;
+console.log(process.env);
 let operatorID = "cenex_operator";
 let messagesEl = document.querySelector(".messages");
 let audioEl = document.querySelector(".remote-audio");
